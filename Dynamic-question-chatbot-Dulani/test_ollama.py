2879b6e6e8cef732d546dd56ea477f7712658f98
TanuriD <it23377844@my.sliit.lk>
2025-11-03 15:26:04 +0530

    Delete Dynamic-question-chatbot-Dulani directory

--- Dynamic-question-chatbot-Dulani/test_ollama.py
+++
@@ -1,8 +0,0 @@
-from langchain_ollama import OllamaLLM
-
-# Connect to LLaMA 3 model
-llm = OllamaLLM(model="llama3")
-
-# Simple test
-response = llm.invoke("Hello! Can you tell me a joke?")
-print("Ollama Response:", response)
